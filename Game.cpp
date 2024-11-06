945fc9ed104a526521f3f595797aff64a36542d7
Raja Muhammad Bilal Arshad <bilall.dude@gmail.com>
2024-11-06 23:25:24 +0500

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -25,10 +25,10 @@ int main()
         cout << endl << "YOU HAVE GIVEN WRONG INPUT";
         return 1;
  
-    cout << "\t\t\t\t.......... CHOOSE YOUR LEVELS..........\nLevel 1 \nLevel 2 \nLevel 3 \nLevel 4 \nLevel 5 \nLevel 6 \n";
+    cout << "\t\t\t\t.......... CHOOSE YOUR LEVELS..........\nLevel 1 \nLevel 2 \nLevel 3 \nLevel 4 \nLevel 5 \nLevel 6 \nLevel 7 \nLevel 8 \n";
     cout << "Enter your level: ";
     cin >> LEVEL;
-    if (!(LEVEL > 0 && LEVEL < 7))
+    if (!(LEVEL > 0 && LEVEL < 8))
     {
         cout << "YOU HAVE GIVEN WRONG INPUT";
         return 1;
